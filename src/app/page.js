167c083abeaf9964a6d9f3f2fd84763e8dec9503
umairtech12 <umairshahbaz988@gmail.com>
2025-04-11 06:16:10 +0500

    venn diagram created

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import supabase from "@/lib/supabase"
 import dynamic from "next/dynamic"
 import FileUploader from "@/components/FileUploader"
+import ProximityChart from "@/components/ProximityChart"
 
 // Dynamically import the CustomHeatmap component with no SSR
 const CustomHeatmap = dynamic(() => import("@/components/CustomHeatmap"), {
@@ -21,12 +22,14 @@ export default async function Home() {
     const hour = timestamp.getHours()
     return hour >= 6 && hour <= 22 // 6 AM to 10 PM
   })
+  console.log(filteredData);
 
   return (
     <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-6xl mx-auto">
         <div className="mb-10">
-        <FileUploader/>
+        {/* <FileUploader/> */}
+        <ProximityChart data={filteredData}/>
 
         </div>
         <CustomHeatmap sensorData={filteredData} />
